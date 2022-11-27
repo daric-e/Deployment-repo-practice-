8b42a9fb16fda630895bb82d3d0abb6852d09a9b
daric-e <100322530+daric-e@users.noreply.github.com>
2022-11-27 16:23:49 -0700

    Innitial commit/ Getting ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import './App.css';
+import NoMatch from './NoMatch'
+import BackButton from "./BackButton";
+import ForwardButton from "./FowardButton";
+import GoHomeButton from "./GoHomeButton";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App"></div>
+//     </Router>
+//   );
+// }
+
+
+function Home() {
+  return <p>Home</p>;
+}
+
+function About() {
+  return <p>About</p>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      < BackButton/>
+      < ForwardButton/>
+      < GoHomeButton/>
+        <div className="App">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Switch>
+            <Route  exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route>
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </div>
+    </Router>
   );
 }
 
 export default App;
+
+// import React from "react";
+// import "../src/App.css";
+// import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+// import UserProfile from "./UserProfile";
+
+// function Home() {
+//   return <p>Home</p>;
+// }
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <div>
+//           <Link to="/">Home</Link>
+//         </div>
+//         {Array(10)
+//           .fill()
+//           .map((ignoredValue, index) => index + 1)
+//           .map((id) => (
+//             <div key={id}>
+//               <Link to={`/user/${id}`}>User {id}</Link>
+//             </div>
+//           ))}
+//         <Switch>
+//           <Route exact={true} path="/">
+//             <Home />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
